da203a03fe0151ce4c12304c3ae3cc8f8dcd366e
unknown <kamit7102@gmail.com>
2023-02-03 20:22:34 +0530

    RelationalOperators

---
+++ Relationaloperators.js
@@ -0,0 +1,102 @@
+//problem-1
+//hello
+// world
+// by using single console.log;
+
+// new line operator
+// "\n" ---> the compiler print the other elements to the next listeners
+
+console.log("hello" + "\n" + "world");
+
+//problem-2
+
+console.log(2<3); // true
+
+console.log(4<4); // false
+
+console.log(4<1); // false
+
+//problem-3
+console.log(3<=2); // false.
+
+console.log(6<=6); // true.
+
+console.log(1<=10000); //  true.
+
+//problem-4
+console.log(2>3); // false.
+
+console.log(2>2) // false.
+
+console.log(3>1); // true.
+
+console.log(2>=2); // true.
+
+console.log(5>=1); // true.
+
+console.log(1>=6); // false.
+
+//problem-5
+
+let goku_age= 18; // left
+
+let legal_driving_age= 21; // right.
+
+// true.
+
+let license = goku_age >= legal_driving_age;
+
+console.log(license);
+
+//problem-6
+let a= 10;
+
+let b= 20;
+
+
+let c= "luffy";
+
+let d= "10";
+
+let e= "Goku";
+
+let f = "goku";
+
+
+console.log(a==b); // false.
+
+console.log(c==c); // true.
+
+console.log(e==f); // false.
+
+//problem-7
+
+console.log(4!=5) //true.
+
+console.log("goku" == "vegita"); // false.
+
+console.log("goku" != "vegita"); // true.
+
+console.log(5==5); // true.
+
+console.log(4 !=4); // false.
+
+//problem-8
+
+console.log(3===3); // true.
+
+
+console.log("2"=== 4); 
+
+console.log("masai" ==="Masai"); // false
+
+
+
+
+
+
+
+
+
+
+
