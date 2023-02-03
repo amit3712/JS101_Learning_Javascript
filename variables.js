009df27136955313de52a78f118a267bdd6276e4
unknown <kamit7102@gmail.com>
2023-02-03 17:02:57 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,82 @@
+//problem-1
+// 1st way
+let x= 10; // decalring a variable and assigning a value 10 to it.
+console.log(x);
+
+//2nd way.
+let a; // declaring a variable a
+a=20; // assigning a value 20 to the variable.
+console.log(a); // printing the variable.
+
+//problem-2
+let a=23;
+console.log(a);
+
+//problem-3
+let number= 100;
+console.log(number); //number= 100;
+
+number= 12345;
+console.log(number); // number= 12345;
+
+//problem-4
+// decalaring a variable name using let word.
+
+// they are other 2 way of declaring a variable.
+
+// var , const.
+
+1)var a= 10; // declaring a variable with name a and assigning a value 10;
+console.log(a);
+
+2)const b= 20; // declaring a variable with name b and assigning a value 10;
+console.log(b);
+
+
+//problem-5
+// why do we have three ways of declaring a variable.
+
+const adharcardnumber= 12345;
+console.log(adharcardnumber);
+
+//problem-6
+// data can be anything ?
+// what if I want to store a name or any other data except number.
+
+// string--- ""
+
+let a= 10; // data is number;
+let name= "goku"; // data is string;
+console.log(name);
+
+//problem-7
+let age= 24; // data is number;
+
+let number= "24"; // data is string;
+
+// in order to check what is the data type inside the varialbe.
+
+console.log(typeof(age));
+console.log(typeof(number));
+
+//problem-8
+// 1. create a varialbe with let and assign a value to it .
+// output.
+let x= 432;
+console.log(x);
+
+//2. create another variable with const and assign a value to it.
+const name= "luffy";
+console.log(typeof(name));
+
+// what is the datatype of const variable.
+
+//problem-9
+let a= 10;
+let b= 20;
+
+// console.log(a);
+// console.log(b);
+
+console.log(a,b);
+
